292a8ad1b6260255cf1b4c81395f979105dde4be
gagaclub <100audgus@gmail.com>
2023-04-06 10:50:46 +0900

    first commit

---
+++ src/main/java/com/project01/mk01/Mk01Application.java
@@ -0,0 +1,13 @@
+package com.project01.mk01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mk01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mk01Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/project01/mk01/Mk01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project01.mk01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mk01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
